30107ef6428b4cfb318e081b8a7bc388a24587d5
saiya_mg <saiya_mg@0cb333e0-7576-4208-ba23-bb5a7c56cba0>
2009-08-07 15:23:50 +0000

    - finished capitalization task

--- uman/src/plugins/preparatory/QUPreparatoryTask.cpp
+++ uman/src/plugins/preparatory/QUPreparatoryTask.cpp
@@ -75,6 +75,9 @@ void QUPreparatoryTask::startOn(QUSongInterface *song) {
 }
 
 QList<QUSmartSetting*> QUPreparatoryTask::smartSettings() const {
+	QSettings settings;
+	QString timeDiffLower = settings.value("timeDiffLower", 15).toString();
+
 	if(_smartSettings.isEmpty()) {
 		switch(_mode) {
 		case AutoAssignFiles:
@@ -86,9 +89,12 @@ QList<QUSmartSetting*> QUPreparatoryTask::smartSettings() const {
 				_smartSettings.append(new QUSmartCheckBox("preparatory/removeUnsupportedTags_" + unsupportedTag, unsupportedTag, true));
 			break;
 		case FixAudioLength:
-			QSettings settings;
-			QString timeDiffLower = settings.value("timeDiffLower", 15).toString();
 			_smartSettings.append(new QUSmartInputField("preparatory/fixAudioLength", timeDiffLower, new QRegExpValidator(QRegExp("\\d*"), 0), "Buffer:", "seconds"));
+			break;
+		case FixCapitalization:
+			_smartSettings.append(new QUSmartCheckBox("preparatory/fixCapitalization_onlyEnglish", tr("Capitalize English songs only"), true));
+			_smartSettings.append(new QUSmartCheckBox("preparatory/fixCapitalization_allCaps", tr("Capitalize each word"), false));
+			_smartSettings.append(new QUSmartCheckBox("preparatory/fixCapitalization_onlyFirstWord", tr("Capitalize first word only"), false));
 			break;
 		}
 	}
@@ -121,8 +127,12 @@ void QUPreparatoryTask::autoSetFiles(QUSongInterface *song, const QString &cover
  * See http://aitech.ac.jp/~ckelly/midi/help/caps.html for the rules.
  */
 void QUPreparatoryTask::fixCapitalization(QUSongInterface *song) {
+	bool onlyEnglish = smartSettings().at(0)->value().toBool();
+	bool allCaps = smartSettings().at(1)->value().toBool();
+	bool onlyFirstWord = smartSettings().at(2)->value().toBool();
+
 	QStringList l = QString("english englisch american us").split(" ");
-	if(!song->language().isEmpty() && !l.contains(song->language(), Qt::CaseInsensitive)) {
+	if(onlyEnglish && !song->language().isEmpty() && !l.contains(song->language(), Qt::CaseInsensitive)) {
 		song->log(tr("Capitalization fix not applicable due to non-english song: \"%1 - %2\"").arg(song->artist()).arg(song->title()), QU::Warning);
 		return;
 	}
@@ -139,29 +149,35 @@ void QUPreparatoryTask::fixCapitalization(QUSongInterface *song) {
 	QStringList conjunctions = QString("and but or nor").split(" ");
 	QStringList prepositions = QString("as at by for from in into of off on onto out over to up with").split(" ");
 
-	for(int i = 0; i < words.size(); i++) {
-		// STEP 1 - Capitalize each word
-		words[i][0] = words[i][0].toUpper();
+	if(!onlyFirstWord) {
+		for(int i = 0; i < words.size(); i++) {
+			// STEP 1 - Capitalize each word
+			words[i][0] = words[i][0].toUpper();
 
-		// STEP 2 - Lower-case for articles, conjunctions and short prepositions
-		if(articles.contains(words[i], Qt::CaseInsensitive) ||
-		   conjunctions.contains(words[i], Qt::CaseInsensitive) ||
-		   prepositions.contains(words[i], Qt::CaseInsensitive))
-			words[i][0] = words[i][0].toLower();
+			// STEP 2 - Lower-case for articles, conjunctions and short prepositions
+			if(!allCaps && (
+			   articles.contains(words[i], Qt::CaseInsensitive) ||
+			   conjunctions.contains(words[i], Qt::CaseInsensitive) ||
+			   prepositions.contains(words[i], Qt::CaseInsensitive)))
+				words[i][0] = words[i][0].toLower();
+		}
 	}
 
 	// STEP 3 - Capitalize "phrasal verbs"
 	QStringList phrasalVerbs = QString("Beat Up,Blow Out,Break Down,Break Into,Break Up,Bring Up,Call Off,Call On,Call Up,Carry On,Come Back,Come Down,Come On,Come Out,Come Over,Do Over,Fill In,Fill Out,Find Out,Get Along,Get Around,Get By,Get Over,Get Through,Get Up,Give Back,Give Up,Go Along,Go Away,Go On,Go Over,Hand In,Hang Up,Hold On,Keep On,Keep Up,Leave Out,Let Down,Look For,Look Into,Look Like,Look Out,Look Over,Look Up,Make Out,Make Up,Pack Up,Pass Out,Pick Out,Pick Up,Put Away,Put Off,Put On,Put Out,Put Up,Roll Over,Run Into,Run Out,Run Over,Show Up,Take After,Take Back,Take Off,Take On,Take Up,Talk Back,Talk Over,Throw Away,Try On,Turn Down,Turn In,Turn Off,Turn On,Use Up,Wait On").split(",");
-	for(int i = 0; i < words.size() - 1; i++) {
-		if(phrasalVerbs.contains(QString("%1 %2").arg(words[i]).arg(words[i+1]), Qt::CaseInsensitive)) {
-			words[i][0] = words[i][0].toUpper();
-			words[i+1][0] = words[i+1][0].toUpper();
+	if(!onlyFirstWord) {
+		for(int i = 0; i < words.size() - 1; i++) {
+			if(phrasalVerbs.contains(QString("%1 %2").arg(words[i]).arg(words[i+1]), Qt::CaseInsensitive)) {
+				words[i][0] = words[i][0].toUpper();
+				words[i+1][0] = words[i+1][0].toUpper();
+			}
 		}
 	}
 
 	// LAST STEP - Capitalize first and last word again
 	words.first()[0] = words.first()[0].toUpper();
-	words.last()[0] = words.last()[0].toUpper();
+	if(!onlyFirstWord)
+		words.last()[0] = words.last()[0].toUpper();
 
 	if(suffixes.isEmpty())
 		song->setInfo(TITLE_TAG, words.join(" "));
