3d869bf67a2cd7e0ba21f12d93815d4cda7b3a83
bohning <bohning@0cb333e0-7576-4208-ba23-bb5a7c56cba0>
2011-10-17 13:29:29 +0000

    changed the lyrics display to be handled by QTextStream and QTextCodec depending on the file encoding

--- uman/src/support/QUTextDialog.cpp
+++ uman/src/support/QUTextDialog.cpp
@@ -3,6 +3,8 @@
 #include <QFile>
 #include <QStringList>
 #include <QRegExp>
+#include <QTextStream>
+#include <QTextCodec>
 
 QUTextDialog::QUTextDialog(QUSongFile *song, QWidget *parent, bool showLyrics): QDialog(parent) {
 	setupUi(this);
@@ -57,11 +59,23 @@ void QUTextDialog::showFile(QUSongFile *song) {
  * Opens the song file and reads the content.
  */
 void QUTextDialog::initFile(QUSongFile *song) {
-	QFile f(song->songFileInfo().filePath());
+	QFile file(song->songFileInfo().filePath());
 
-	if(f.open(QIODevice::ReadOnly | QIODevice::Text)) {
-                //QStringList lines(QString::fromLocal8Bit(f.readAll()).split("\n"));
-                QStringList lines(QString::fromUtf8(f.readAll()).split("\n"));
+	if(file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+		QTextStream in;
+		in.setDevice(&file);
+
+		if(song->encoding() == "UTF8") {
+			in.setCodec(QTextCodec::codecForName("UTF-8"));
+		} else if (song->encoding() == "CP1252") {
+			in.setCodec(QTextCodec::codecForName("windows-1252"));
+		} else if (song->encoding() == "CP1250") {
+			in.setCodec(QTextCodec::codecForName("windows-1250"));
+		} else {
+			// should not occur
+		}
+
+		QStringList lines = in.readAll().split("\n");
 
 		for(int i = 0; i < lines.size(); i++) { // format line numbers
 			lines[i].prepend(QString("<font color=#808080>%1 </font>").arg(i + 1, 3, 10, QChar('0')));
@@ -73,7 +87,7 @@ void QUTextDialog::initFile(QUSongFile *song) {
 		content.replace(QRegExp("(#[^<]*:)"), "<b>\\1</b>");
 
 		textEdit->insertHtml(content);
-		f.close();
+		file.close();
 	}
 
 	textEdit->moveCursor(QTextCursor::Start);
