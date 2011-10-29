c649ff2359be4923fdd3a95a6fb7efacc988cb79
bohning <bohning@0cb333e0-7576-4208-ba23-bb5a7c56cba0>
2011-10-29 07:29:19 +0000

    default action in context menu is bold now

--- uman/src/songtree/QUSongTree.cpp
+++ uman/src/songtree/QUSongTree.cpp
@@ -1268,25 +1268,27 @@ void QUSongTree::keyPressEvent(QKeyEvent *event) {
 
 QMenu* QUSongTree::itemMenu(QUSongItem *item) {
 	QMenu *menu = new QMenu(this);
+	QFont *font = new QFont();
+	font->setBold(true);
 
 	if(item && !item->isToplevel()) {
 		// file menu
 		if(item->text(0) == item->song()->cover() or item->text(0) == item->song()->background()) {
-			menu->addAction(QIcon(":/types/image_show.png"), tr("Show preview"), this, SLOT(openCurrentFileInternal()), Qt::Key_Enter);
+			menu->addAction(QIcon(":/types/image_show.png"), tr("Show preview"), this, SLOT(openCurrentFileInternal()), Qt::Key_Enter)->setFont(*font);
 			menu->addAction(QIcon(":/types/image_openexternal.png"), tr("Open image file externally"), this, SLOT(openCurrentFileExternal()));
 		} else if(item->text(0) == item->song()->video()) {
-			menu->addAction(QIcon(":/types/video_openexternal.png"), tr("Open video file externally"), this, SLOT(openCurrentFileExternal()), Qt::Key_Enter);
+			menu->addAction(QIcon(":/types/video_openexternal.png"), tr("Open video file externally"), this, SLOT(openCurrentFileExternal()), Qt::Key_Enter)->setFont(*font);;
 		} else if(item->text(0) == item->song()->mp3()) {
-			menu->addAction(QIcon(":/types/music_openexternal.png"), tr("Open audio file externally"), this, SLOT(openCurrentFileExternal()), Qt::Key_Enter);
+			menu->addAction(QIcon(":/types/music_openexternal.png"), tr("Open audio file externally"), this, SLOT(openCurrentFileExternal()), Qt::Key_Enter)->setFont(*font);;
 		} else if(item->text(0) == item->song()->txt()) {
-			menu->addAction(QIcon(":/types/text.png"), tr("Show file contents"), this, SLOT(openCurrentFileInternal()), Qt::Key_Enter);
+			menu->addAction(QIcon(":/types/text.png"), tr("Show file contents"), this, SLOT(openCurrentFileInternal()), Qt::Key_Enter)->setFont(*font);;
 			menu->addAction(QIcon(":/types/text_openexternal.png"), tr("Open text file externally"), this, SLOT(openCurrentFileExternal()));
 		} else if(item->song()->isFriend(item->text(0))) {
-			menu->addAction(QIcon(":/types/text.png"), tr("Show file contents"), this, SLOT(openCurrentFileInternal()), Qt::Key_Enter);
+			menu->addAction(QIcon(":/types/text.png"), tr("Show file contents"), this, SLOT(openCurrentFileInternal()), Qt::Key_Enter)->setFont(*font);;
 			menu->addAction(QIcon(":/types/text_openexternal.png"), tr("Open text file externally"), this, SLOT(openCurrentFileExternal()));
 			menu->addAction(QIcon(":/types/text_setprimary.png"), tr("Set as primary song"), this, SLOT(setCurrentFilePrimary()));
 		} else {
-			menu->addAction(QIcon(":/types/file.png"), tr("Open externally"), this, SLOT(openCurrentFileExternal()), Qt::Key_Enter);
+			menu->addAction(QIcon(":/types/file.png"), tr("Open externally"), this, SLOT(openCurrentFileExternal()), Qt::Key_Enter)->setFont(*font);;
 		}
 		menu->addAction(QIcon(":/control/bin.png"), tr("Delete"), this, SLOT(deleteCurrentItem()), Qt::Key_Delete);
 	} else {
